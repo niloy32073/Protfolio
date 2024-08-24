854d1dd8b27b2abdc19410f41800759c68e4e5da
niloy32073 <mdnrahmanx27@gmail.com>
2024-08-24 23:50:46 +0600

    Update

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter basename="/<your-repo-name>">
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
